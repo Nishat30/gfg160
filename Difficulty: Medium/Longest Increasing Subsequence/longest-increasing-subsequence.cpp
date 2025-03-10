2f8ad7147ff00b7804d0937af4302620f8d8865c
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-10 06:13:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -7,24 +7,21 @@ using namespace std;
 
 class Solution {
   public:
-    int dp[1005];
-    int fn(vector<int>&a, int i){
-        if(i>=a.size())return 0;
-        if(dp[i]!=-1)return dp[i];
-        int ans=1;
-        for(int j=i;j<a.size();j++){
-            if(a[j]>a[i])ans=max(ans,fn(a,j)+1);
-        }
-        return dp[i]=ans;
-    }
     int lis(vector<int>& a) {
         // code here
-        int ans=0;
-        memset(dp,-1,sizeof(dp));
+        vector<int> temp;
+        int len=1;
+        temp.push_back(a[0]);
         for(int i=0;i<a.size();i++){
-            ans=max(ans,fn(a,i));
+            if(a[i]>temp.back()){
+                temp.push_back(a[i]);
+                len++;
+            }else{
+                int idx=lower_bound(temp.begin(),temp.end(),a[i])-temp.begin();
+                temp[idx]=a[i];
+            }
         }
-        return ans;
+        return len;
     }
 };
 
