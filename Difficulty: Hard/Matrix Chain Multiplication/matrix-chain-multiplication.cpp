cf2b73831487bc6d82486e05a381a8ae3b04532b
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-24 22:16:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 // } Driver Code Ends
 
-// User function Template for C++
-
 class Solution {
   public:
     int f(int i,int j,vector<int>& arr,vector<vector<int>>& dp){
@@ -22,8 +20,22 @@ class Solution {
     int matrixMultiplication(vector<int> &arr) {
         // code here
         int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return f(1,n-1,arr,dp);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        // return f(1,n-1,arr,dp);
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 0;
+        }
+        for(int i=n-1;i>0;i--){
+            for(int j=i+1;j<n;j++){
+                int mini=1e9;
+                for(int k=i;k<j;k++){
+                    int steps= arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                    if(steps<mini) mini=steps;
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][n-1];
     }
 };
 
