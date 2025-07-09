f8ea7b2be75de7471d50ad17fd9f4ff9fbe37a19
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-07-10 03:31:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.cpp
@@ -8,42 +8,32 @@ using namespace std;
 
 class Solution {
   public:
+    bool static comp(pair<int,int> job1,pair<int,int> job2){
+        return (job1.second>job2.second);
+    }
     vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) {
         // code here
-        int n = deadline.size();
-        vector<pair<int, int>> jobs(n);
-
-        int maxDays = 0;
-        for (int i = 0; i < n; i++) {
-            jobs[i] = {profit[i], deadline[i]};
-            maxDays = max(maxDays, deadline[i]);
+        int n=deadline.size();
+        vector<pair<int,int>> job;
+        int max_dead=0;
+        for(int i=0;i<n;i++){
+            job.push_back({deadline[i],profit[i]});
+            max_dead=max(max_dead,deadline[i]);
         }
-
-        sort(jobs.rbegin(), jobs.rend()); // Sort jobs in decreasing order of profit
-
-        vector<int> parent(maxDays + 1);
-        for (int i = 0; i <= maxDays; i++) parent[i] = i; // Initialize disjoint set
-
-        // Disjoint Set Find Function with Path Compression
-        function<int(int)> find = [&](int day) {
-            if (parent[day] == day) return day;
-            return parent[day] = find(parent[day]);
-        };
-
-        int jobCount = 0, maxProfit = 0;
-
-        for (auto& job : jobs) {
-            int p = job.first, d = job.second;
-
-            int availableDay = find(d); // Find closest available day
-            if (availableDay > 0) {
-                parent[availableDay] = find(availableDay - 1); // Update parent (union)
-                jobCount++;
-                maxProfit += p;
+        sort(job.begin(),job.end(),comp);
+        vector<int> slot(max_dead+1,-1);
+        int countJobs = 0, jobProfit = 0;
+        for(int i=0;i<n;i++){
+            for (int j = job[i].first; j > 0; j--) {
+            if (slot[j] == -1) {
+               slot[j] = i;
+               countJobs++;
+               jobProfit += job[i].second;
+               break;
             }
+         }
         }
-
-        return {jobCount, maxProfit};
+        return {countJobs,jobProfit};
     }
 };
 
