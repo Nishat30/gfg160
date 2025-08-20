ba02c4a624a3566af5f815a28fe792d2a961be67
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-08-20 23:56:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in fully rotated sorted 2D matrix/search-in-fully-rotated-sorted-2d-matrix.cpp
+++ Difficulty: Medium/Search in fully rotated sorted 2D matrix/search-in-fully-rotated-sorted-2d-matrix.cpp
@@ -2,42 +2,33 @@ class Solution {
   public:
     bool searchMatrix(vector<vector<int>> &mat, int x) {
         // code here
-        int m = mat.size();
-        int n = mat[0].size();
-        int i = 0;
-        int j = (n*m)-1;
-        while(i<=j){
-            int mid = (i+j)/2;
-            int lowr = i/n;
-            int lowc = i%n;
-            int highr = j/n;
-            int highc = j%n;
-            int midr = mid/n;
-            int midc = mid%n;
-            if(mat[midr][midc]==x) return true;
-            if(mat[lowr][lowc]<=mat[midr][midc]){
-                
-            
-                if(mat[lowr][lowc]<=x && mat[midr][midc]>=x){
-                    j = mid-1;
-                }   
-                else {
-                    i = mid+1;
-                }
-            
-                
-                
+        int n = mat.size();
+        int m = mat[0].size();
+        bool flag = false;
+        vector<int> rows;
+
+    // collect all candidate rows
+        for (int i = 0; i < n; i++) {
+            sort(mat[i].begin(),mat[i].end());
+            if (mat[i][0] <= x && mat[i][m - 1] >= x) {
+                flag = true;
+                rows.push_back(i);
             }
-            else{
-                if(mat[midr][midc]<=x && x<=mat[highr][highc]){
-                    i = mid+1;
-                }
-                else{
-                    j=mid-1;
-                }
-            }
-            
         }
+
+        if (!flag) return false;
+
+    // check each candidate row using binary search
+        for (int r : rows) {
+            int start = 0, end = m - 1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (mat[r][mid] == x) return true;
+                else if (mat[r][mid] > x) end = mid - 1;
+                else start = mid + 1;
+            }
+        }
+
         return false;
     }
 };
