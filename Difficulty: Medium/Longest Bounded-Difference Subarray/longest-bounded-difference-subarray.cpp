0da6c652d2bec1bd23d9c861245361b2b08f75c0
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-09-28 20:43:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
+++ Difficulty: Medium/Longest Bounded-Difference Subarray/longest-bounded-difference-subarray.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 
 class Solution {
   public:
@@ -35,33 +29,3 @@ class Solution {
     }
 };
 
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.longestSubarray(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
