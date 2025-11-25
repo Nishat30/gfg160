703287cbc434e189b6cc833a60b64e4e81ce51a5
Nishat Khanam <159575377+Nishat30@users.noreply.github.com>
2025-11-26 01:45:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/AND In Range/and-in-range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int andInRange(int l, int r) {
+        // code here
+        int ans = l;
+        int diff = r-l;
+        int bit = 1;
+        while(bit<=diff){
+            if(ans&bit){
+                ans = ans&~(bit);
+            }
+            bit<<=1;
+        }
+        return ans&r;
+    }
+};
